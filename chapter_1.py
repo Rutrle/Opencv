e2b1bdede4110974825076e6b54398f2f060f277
Jakub <rutrle@seznam.cz>
2020-11-22 15:04:45 +0100

    opening a video with opencv

--- chapter_1.py
+++ chapter_1.py
@@ -1,7 +1,18 @@
 import cv2
 
-
+''' open a picture
 img = cv2.imread('Resources/lena.png')
 
 cv2.imshow("Output", img)
 cv2.waitKey(0)
+'''
+
+cap = cv2.VideoCapture('Resources/test_video.mp4')
+
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Video", img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
