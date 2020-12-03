9988f48c8ec9899be125639b9e5bf7fa14e66762
Jakub <rutrle@seznam.cz>
2020-12-04 00:43:59 +0100

    plotting line

--- chapter_4/shapes.py
+++ chapter_4/shapes.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 
 img = np.zeros((512, 512, 3), np.uint8)
-print(img)
-img[200:300, 100:300] = 255, 0, 0
+# print(img)
+#img[:] = 255, 0, 0
+
+cv2.line(img, (0, 0), (300, 300), (0, 255, 0), 3)
 
 cv2.imshow("Image", img)
 
