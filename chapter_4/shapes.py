0b17072524d76feee59c60248e41e95539518ba2
Jakub <rutrle@seznam.cz>
2020-12-07 22:49:41 +0100

    plotting circle

--- chapter_4/shapes.py
+++ chapter_4/shapes.py
@@ -9,6 +9,8 @@ cv2.line(img, (0, 0), (img.shape[1], img.shape[0]), (0, 255, 0), 3)
 
 cv2.rectangle(img, (0, 0), (250, 350), (0, 0, 255), cv2.FILLED)
 
+cv2.circle(img, (400, 50), 30, (255, 255, 0), 5)
+
 cv2.imshow("Image", img)
 
 cv2.waitKey(0)
