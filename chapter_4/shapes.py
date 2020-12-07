01bc4a3aadfba0a54c748d7d17ded21f89504794
Jakub <rutrle@seznam.cz>
2020-12-07 22:53:03 +0100

    adding text

--- chapter_4/shapes.py
+++ chapter_4/shapes.py
@@ -11,6 +11,9 @@ cv2.rectangle(img, (0, 0), (250, 350), (0, 0, 255), cv2.FILLED)
 
 cv2.circle(img, (400, 50), 30, (255, 255, 0), 5)
 
+cv2.putText(img, " OPENCV ", (300, 200),
+            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 150, 0), 1)
+
 cv2.imshow("Image", img)
 
 cv2.waitKey(0)
